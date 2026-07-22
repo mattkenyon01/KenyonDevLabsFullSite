5c7873a522cd899e397f113b0f1b300e37f7698f
MattKenyon03 <165806104+mattkenyon01@users.noreply.github.com>
2026-07-22 20:38:50 +0100

    Wide screen image moved

--- obj/Debug/net8.0/KenyonDevLabs.AssemblyInfo.cs
+++ obj/Debug/net8.0/KenyonDevLabs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KenyonDevLabs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016faec2664cc4381b41b912a5cab8830a01393b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2383040e061d199faa24145c0a94b4a0f2f680f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KenyonDevLabs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KenyonDevLabs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
